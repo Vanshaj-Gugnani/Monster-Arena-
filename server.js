3a0be041c93b2726b8376c621d22c90311f796cc
vgugnani <vanshajgugnani04@gmail.com>
2024-09-30 20:31:08 -0400

    integrity declaration

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+/********************************************************************************
+* WEB322 – Assignment 02
+*
+* I declare that this assignment is my own work in accordance with Seneca's
+* Academic Integrity Policy:
+*
+* https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+*
+* Name: Vanshaj Gugnani Student ID: vgugnani@myseneca.ca Date: 30-sep-2024
+*
+* Published URL: https://webassignment2-ten.vercel.app/
+*
+********************************************************************************/
+
+
+
+
 const express = require('express')
 const app = express();
 const legoData = require("./modules/legoSets");
 legoData.initialize();
+
+
 app.get('/', (req, res)=>{
     res.send('Assignment 2: Vanshaj Gugnani - 155445224')
 })
